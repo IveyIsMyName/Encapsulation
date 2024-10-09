642788db0192e12b6a351cda16e3de812d984265
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-10-09 11:22:49 -0400

    Add project files.

---
+++ Encapsulation/Point.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+	internal class Point
+	{
+		#region ENCAPS1
+		//double x, y;
+		//public double X //Property
+		//{
+		//	get
+		//	{
+		//		return x;
+		//	}
+		//	set
+		//	{
+		//		if (value > 200) value = 200;
+		//		x = value;
+		//	}
+		//}
+		//public double Y
+		//{
+		//	get
+		//	{
+		//		return y;
+		//	}
+		//	set
+		//	{
+		//		if (value > 150) value = 150;
+		//		y = value;
+		//	}
+		//}
+		//public double GetX()
+		//{
+		//	return x;
+		//}
+		//public double GetY()
+		//{
+		//	return y;
+		//}
+		//public void SetX(double x)
+		//{
+		//	if (x > 200) x = 200;
+		//	this.x = x;
+		//}
+		//public void SetY(double y)
+		//{
+		//	if (y > 150) y = 150;
+		//	this.y = y;
+		//} 
+		#endregion
+
+		public double X { get; set; } // Автосвойства
+		public double Y { get; set; }
+
+		public Point(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+		public void Print()
+		{
+			Console.WriteLine($"X = {X}\tY = {Y}");
+		}
+		public double Distance(Point other)
+		{
+			double x_distance = this.X - other.X;
+			double y_distance = this.Y - other.Y;
+			double distance = Math.Sqrt(x_distance * x_distance + y_distance * y_distance);
+			return distance;
+		}
+		public static double Distance(Point A, Point B)
+		{
+			double x_distance = A.X - B.X;
+			double y_distance = A.Y - B.Y;
+			double distance = Math.Sqrt(x_distance * x_distance + y_distance * y_distance);
+			return distance;
+		}
+
+	}
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,37 @@
+﻿//#define STRUCT_POINT
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+#if STRUCT_POINT
+			Point A = new Point(22, 33);
+			//Console.WriteLine($"X = {A.GetX()}\tY = {A.GetY()}");
+			//A.SetX(2000);
+			//A.SetY(3000);
+			//A.Print();
+			//A.X = 22;
+			//A.Y = 33;
+			A.Print(); 
+#endif
+			Point A = new Point(2, 3);
+			Point B = new Point(7, 8);
+
+			A.Print();
+			B.Print();
+
+            Console.WriteLine($"Расстояние от точки 'A' до точки 'B':{A.Distance(B)}");
+            Console.WriteLine($"Расстояние от точки 'B' до точки 'A':{B.Distance(A)}");
+            Console.WriteLine($"Расстояние между точками 'A' и 'B':{Point.Distance(A, B)}");
+            Console.WriteLine($"Расстояние между точками 'B' и 'A':{Point.Distance(B, A)}");
+		}
+	}
+}

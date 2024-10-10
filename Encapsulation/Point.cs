635e4a2d251b1fdea5414c7514f5f752074a0eff
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-10-10 13:18:33 -0400

    Operator overloading DONE

--- Encapsulation/Point.cs
+++ Encapsulation/Point.cs
@@ -58,10 +58,55 @@ namespace Encapsulation
 		public double X { get; set; } // Автосвойства
 		public double Y { get; set; }
 
+		public Point()
+		{
+			Console.WriteLine($"DefaultConstructor:{this.GetHashCode()}");
+		}
 		public Point(double x, double y)
 		{
 			X = x;
 			Y = y;
+			Console.WriteLine($"Constructor:{this.GetHashCode()}");
+		}
+		public Point(Point other)
+		{
+			this.X = other.X;
+			this.Y = other.Y;
+			Console.WriteLine($"CopyConstructor:{this.GetHashCode()}");
+		}
+		~Point()
+		{
+			Console.WriteLine($"Destructor:{this.GetHashCode()}");
+		}
+		public static Point operator +(Point left, Point right)
+		{
+			Point res = new Point();
+			res.X = left.X + right.X;
+			res.Y = left.Y + right.Y;
+			return res;
+		}
+		public static Point operator -(Point left, Point right)
+		{
+			return new Point
+			(
+				left.X - right.X,
+				left.Y - right.Y
+			);
+		}
+		public static Point operator++(Point obj)
+		{
+			obj.X++;
+			obj.Y++;
+			return obj;
+		}
+		public static bool operator ==(Point left, Point right)
+		{
+			return left.X == right.X && left.Y == right.Y;
+		}
+		public static bool operator !=(Point left, Point right)
+		{
+			return left.X != right.X && left.Y != right.Y;
+			//return !(left == right);
 		}
 		public void Print()
 		{
--- Encapsulation/Program.cs
+++ Encapsulation/Program.cs
@@ -1,4 +1,5 @@
 ﻿//#define STRUCT_POINT
+//#define DISTANCE
 
 using System;
 using System.Collections.Generic;
@@ -22,16 +23,35 @@ namespace Encapsulation
 			//A.Y = 33;
 			A.Print(); 
 #endif
+#if DISTANCE
 			Point A = new Point(2, 3);
-			Point B = new Point(7, 8);
-
+			Point B = new Point();      //Default Constructor
+			B.X = 3;
+			B.Y = 4;
+			Point C = new Point(B);
 			A.Print();
 			B.Print();
+			C.Print();
+			Point D;
+			D = C;
 
-            Console.WriteLine($"Расстояние от точки 'A' до точки 'B':{A.Distance(B)}");
-            Console.WriteLine($"Расстояние от точки 'B' до точки 'A':{B.Distance(A)}");
-            Console.WriteLine($"Расстояние между точками 'A' и 'B':{Point.Distance(A, B)}");
-            Console.WriteLine($"Расстояние между точками 'B' и 'A':{Point.Distance(B, A)}");
+			Console.WriteLine($"Расстояние от точки 'A' до точки 'B':{A.Distance(B)}");
+			Console.WriteLine($"Расстояние от точки 'B' до точки 'A':{B.Distance(A)}");
+			Console.WriteLine($"Расстояние между точками 'A' и 'B':{Point.Distance(A, B)}");
+			Console.WriteLine($"Расстояние между точками 'B' и 'A':{Point.Distance(B, A)}"); 
+#endif
+			Point A = new Point(2, 3);
+			Point B = new Point(7, 8);
+			Point C = new Point(A + B);	
+			C.Print();
+			Point D = new Point(A - B);
+			D.Print();
+
+			for (Point i = new Point(); i.X < 10; i++)
+			{
+				i.Print();
+			}
+
 		}
 	}
 }
